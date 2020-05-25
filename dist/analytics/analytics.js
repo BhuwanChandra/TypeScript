1cc28216a4c8c7585a087b175978f98e3a72bdc7
BhuwanChandra <bhuwan7779@gmail.com>
2020-05-26 00:55:37 +0530

    started minesweeper game in react

--- dist/analytics/analytics.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log('analytics');
-//# sourceMappingURL=analytics.js.map
--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const user = "Max";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log("Clicked");
-});
-console.log(user);
-//# sourceMappingURL=app.js.map
