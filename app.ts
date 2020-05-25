646dcb8d0e868c2e6b0184ebb49ae6d4e653d4a7
BhuwanChandra <bhuwan7779@gmail.com>
2020-05-25 21:01:17 +0530

    completed basics

--- app.ts
+++ app.ts
@@ -1,1 +1,30 @@
-console.log("This is ts/js file!");
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result;
+  // }
+}
+
+const combineAges = combine(45, 36, "as-number");
+
+console.log(combineAges);
+
+const combineStringAges = combine("45", "33", "as-number");
+
+console.log(combineStringAges);
+
+const combineNames = combine("Max", "Anagel", 'as-text');
+
+console.log(combineNames);
